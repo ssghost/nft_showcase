f701f69f51eddc87abfb5f5ca90868d65934c7d7
ssghost <ssprof0@gmail.com>
2022-06-26 23:01:04 +0800

    update input field

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -2,14 +2,14 @@ import React, { useRef } from "react";
 import "./styles.css";
 
 interface props {
-  name: string;
-  setName: React.Dispatch<React.SetStateAction<string>>;
+  slug: string;
+  setSlug: React.Dispatch<React.SetStateAction<string>>;
   id: number;
-  setId : React.Dispatch<React.SetStateAction<number>>; 
+  setId: React.Dispatch<React.SetStateAction<number>>; 
   handleAdd: (e: React.FormEvent) => void;
 }
 
-const InputField: React.FC<props> = ({ name, setName, id, setId, handleAdd }) => {
+const InputField: React.FC<props> = ({ slug, setSlug, id, setId, handleAdd }) => {
   const inputRef = useRef<HTMLInputElement>(null);
 
   return (
@@ -22,20 +22,20 @@ const InputField: React.FC<props> = ({ name, setName, id, setId, handleAdd }) =>
     >
       <input
         type="text"
-        placeholder="Enter an NFT Name"
-        value={name}
+        placeholder="Enter an NFT slug:"
+        value={slug}
         ref={inputRef}
-        onChange={(e) => setName(e.target.value)}
+        onChange={(e) => setSlug(e.target.value)}
         className="input__box"
       />
       <input
         type="text"
-        placeholder="Enter an NFT Id"
+        placeholder="Enter an NFT token id:"
         value={id}
         ref={inputRef}
         onChange={(e) => setId(Number(e.target.value))}
         className="input__box"
-      />
+      /> 
       <button type="submit" className="input_submit">
         SHOW
       </button>
